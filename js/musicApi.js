aaa52990f087ff4b20b9a8a5be4cbc0c9d78d9f3
BrendonW250 <brendonwright16@gmail.com>
2022-05-01 21:28:42 -0400

    musicApi

--- js/musicApi.js
+++ js/musicApi.js
@@ -1,11 +1,13 @@
 
 document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('.refresh').addEventListener('click', refreshPage)
 
 function getFetch(){
     const yourArtist = document.querySelector('#artist').value
     const artist = document.querySelector('#artist').value
-    let catalog = []
+    
 
+    // artist album and year info api
     const url = "https://theaudiodb.com/api/v1/json/2/discography.php?s="+yourArtist
 
     // artist info api
@@ -18,75 +20,37 @@ function getFetch(){
         .then(data => {
             console.log(data)
 
-            let arr = []
-            data.album.forEach(element => {
-                arr.push(element.strAlbum + " " + element.intYearReleased + " ")
-                
+            // looping through each element in the given array
+            // from the api
 
-           
-                document.querySelector('#album-display').innerHTML = arr
-                
-            });
+            // 
+            function listOfAlbums(){
 
-            
+                let crazy = []
+                // let displayAlbums = document.getElementById('album-display')
 
-            // data.album.forEach(element => {
-            //     // console.log(element.strAlbum + " " + element.intYearReleased)
-            //     document.querySelector('#album').innerHTML = element.strAlbum
-            //     document.querySelector('#album2').innerHTML = element.strAlbum
-                
-             
-            // });
-            // let albumData = data.album
-            // for (let i=0; i<albumData.length; i++){
-            //     console.log(albumData[i])
-            //     document.querySelector('#album').innerHTML = albumData[i].strAlbum
-            //     document.querySelector('#album2').innerHTML = albumData[i].strAlbum
-            // }
-                
+                // 
+                data.album.forEach(element => {
+                    crazy.push((element.strAlbum + " " + element.intYearReleased))
+                    // displayAlbums.innerHTML += "<br>" + crazy[element]
 
+                    
 
-                // console.log(element.strAlbum)
-                // console.log(element.intYearReleased)
-                
-                // document.querySelector('#album2').innerHTML = el2.strAlbum + " " + el2.intYearReleased
-                // document.querySelector('#album3').innerHTML = el3.strAlbum + " " + el3.intYearReleased
-                // document.querySelector('#album4').innerHTML = el4.strAlbum + " " + el4.intYearReleased
-                // document.querySelector('#album5').innerHTML = el5.strAlbum + " " + el5.intYearReleased
-            
+                    // console.log(crazy)
+    
+                })
+                let displayAlbums = document.getElementById('albums')
+                for (let i=0; i<crazy.length; i++){
+                    displayAlbums.innerHTML += "<br>" + crazy[i]
+                    console.log(crazy[i])
+                }
 
-            // potential # of albums
-            // document.querySelector('#album0').innerHTML = data.album[0].strAlbum.intYearReleased
-            // document.querySelector('#album1').innerHTML = data.album[1].strAlbum
-            // document.querySelector('#album2').innerHTML = data.album[2].strAlbum
-            // document.querySelector('#album3').innerHTML = data.album[3].strAlbum
-            // document.querySelector('#album4').innerHTML = data.album[4].strAlbum
-            // document.querySelector('#album5').innerHTML = data.album[5].strAlbum
-            // document.querySelector('#album6').innerHTML = data.album[6].strAlbum
-            // document.querySelector('#album7').innerHTML = data.album[7].strAlbum
-            // document.querySelector('#album8').innerHTML = data.album[8].strAlbum
-            // document.querySelector('#album9').innerHTML = data.album[9].strAlbum
+                // let albumInfo = crazy 
+                // console.log(albumInfo)
+            }
+            listOfAlbums()
 
             
-
-            // for (let i=0; i<data.album; i++){
-            //     console.log(data.album[i].strAlbum)
-            //     document.querySelector('#album1').innerHTML = data.album[i].strAlbum
-            // }
-
-
-            
-
-            // document.querySelector('#album1').innerHTML = "Albums: " + data
-            
-
-            // for (let i=0; i<data.album; i++){
-            //     document.querySelector('#album1').innerHTML = "Albums: " + data.album[i].strAlbum
-            //     document.querySelector('#album2').innerHTML = "Albums: " + data.album[i].strAlbum
-            // }
-
-            // displaying album contents
-            // document.querySelector('#albums').innerHTML = "Albums: " + data.album[0].strAlbum
 
         })
 
@@ -107,12 +71,21 @@ function getFetch(){
                 newArr.push(element.strArtist)
 
             })
-            document.querySelector('.desired-artist').innerHTML = newArr
-            document.querySelector('img').src = data.artists.strArtistThumb
+            document.querySelector('.desired-artist').innerHTML = "Artist: " + newArr
+            document.querySelector('img').src = data.artists[0].strArtistThumb
+        
+            document.getElementById('background-heading').innerHTML = "Career Overview: "
+            document.querySelector('.artist-background').innerHTML = data.artists[0].strBiographyEN
+
+            document.getElementById('catalog-heading').innerHTML = "Catalog: "
         })
 
         .catch(err => {
             console.log(`error${err}`)
         })
+}
+
+function refreshPage(){
+    location.reload()
 }
 
